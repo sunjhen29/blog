ebe8aceb2f8b310c251b41ff16d1e1c9789ba845
Sunday Doctolero <sunjhen29@yahoo.com>
2017-02-22 11:56:55 +0800

    change asset to forward slash

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,8 +6,8 @@
 
     <link href='https://fonts.googleapis.com/css?family=Raleway:400,700' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css">
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('/css/custom.css') }}">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/custom.css">
 </head>
 <body>
     <header>
@@ -57,6 +57,6 @@
         </div>
     </div>
 </footer>
- <script src="{{ asset('/js/bootstrap.min.js') }}"></script>
+ <script src="/js/bootstrap.min.js"></script>
 </body>
 </html>
